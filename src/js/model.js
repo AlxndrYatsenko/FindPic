c5798aa2889b44c3a04275a456e91c952ee811f1
86derya <derya_86@ukr.net>
2018-10-31 01:50:39 +0400

    layout and prev/next buttons/ Most of files edited

--- src/js/model.js
+++ src/js/model.js
@@ -2,46 +2,78 @@ import * as modal from './services/modal'
 
 export default class Model {
 
-	constructor() {
-		this.currentQuery = '';
-		this.currentPage = 1;
-		this.localImages = JSON.parse(localStorage.getItem('images'));
+    constructor() {
+        this.currentQuery = '';
+        this.currentPage = 1;
+        this.localImages = JSON.parse(localStorage.getItem('images'));
 
-		if (this.localImages === null || this.localImages.length === 0) {
-			this.localImages = []
-		}
-	}
+        if (this.localImages === null || this.localImages.length === 0) {
+            this.localImages = []
+        }
+    }
 
-	resetCurrentPage() {
-		this.currentPage = 1;
-	}
+    resetCurrentPage() {
+        this.currentPage = 1;
+    }
 
-	incrementCurrentPage() {
-		this.currentPage += 1;
-	}
+    incrementCurrentPage() {
+        this.currentPage += 1;
+    }
 
-	addToLocalStorage(arr) {
-		const jsonObj = JSON.stringify(arr);
-		localStorage.setItem(`images`, jsonObj);
-	}
+    addToLocalStorage(arr) {
+        const jsonObj = JSON.stringify(arr);
+        localStorage.setItem(`images`, jsonObj);
+    }
 
-	isHasId(id, arr) {
-		return arr.some(obj => (obj.id === id))
-	}
+    isHasId(id, arr) {
+        return arr.some(obj => (obj.id === id))
+    }
 
-	backdropImageInit(selectedImage) {
-		return modal.showCurrentImage(selectedImage);
-	}
+    backdropImageInit(selectedImage) {
+        return modal.showCurrentImage(selectedImage);
+    }
 
-	backdropShowNextImage() {
-		return modal.showNextImage();
-	}
+    backdropShowNextImage() {
+        return modal.showNextImage();
+    }
 
-	backdropShowPrevImage() {
-		return modal.showPrevImage();
-	}
+    backdropShowPrevImage() {
+        return modal.showPrevImage();
+    }
 
-	backdropCloseModal() {
-		return modal.closeModal();
-	}
+    backdropCloseModal() {
+            return modal.closeModal();
+        }
+        // antiScroll=====================================
+    preventDefault(e) {
+        e = e || window.event;
+        if (e.preventDefault)
+            e.preventDefault();
+        e.returnValue = false;
+    }
+
+    preventDefaultForScrollKeys(e) {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    }
+    disableScroll() {
+        if (window.addEventListener) // older FF
+            window.addEventListener('DOMMouseScroll', this.preventDefault, false);
+        window.onwheel = this.preventDefault; // modern standard
+        window.onmousewheel = document.onmousewheel = this.preventDefault; // older browsers, IE
+        window.ontouchmove = this.preventDefault; // mobile
+        document.onkeydown = this.preventDefaultForScrollKeys;
+
+    }
+    enableScroll() {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', this.preventDefault, false);
+        window.onmousewheel = document.onmousewheel = null;
+        window.onwheel = null;
+        window.ontouchmove = null;
+        document.onkeydown = null;
+    }
+
 }
