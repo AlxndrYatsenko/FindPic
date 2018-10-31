710f81c4619694c0b906c9d2885976b94a0b4699
AlxndrYatsenko <oldr.yatsenko@gmail.com>
2018-10-31 02:19:03 +0200

    add build

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,246 +1,249 @@
-import { getImages } from './services/api';
+import {
+	getImages
+} from './services/api';
 
-import { imagesOnThePage } from './services/modal';
+import {
+	imagesOnThePage
+} from './services/modal';
 
 export default class Controller {
 
-    constructor(model, view) {
-        this._model = model;
-        this._view = view;
-        this.images = this._model.localImages;
-        this.imagesOnPage = imagesOnThePage;
+	constructor(model, view) {
+		this._model = model;
+		this._view = view;
+		this.images = this._model.localImages;
+		this.imagesOnPage = imagesOnThePage;
 
-        this._view.refs.body.addEventListener('click',
-						this.handleModalFalseClick.bind(this));
-						
-        this._view.refs.siteLogo.addEventListener('click',
-            this.handleLogoClick.bind(this));
+		this._view.refs.body.addEventListener('click',
+			this.handleModalFalseClick.bind(this));
 
-        this._view.refs.form.addEventListener('submit',
-            this.handleFormSumit.bind(this));
+		this._view.refs.siteLogo.addEventListener('click',
+			this.handleLogoClick.bind(this));
 
-        this._view.refs.loadMoreBtn.addEventListener('click',
-            this.handleLoadMoreClick.bind(this));
+		this._view.refs.form.addEventListener('submit',
+			this.handleFormSumit.bind(this));
 
-        this._view.refs.list.addEventListener('click',
-            this.handleOpenModal.bind(this));
+		this._view.refs.loadMoreBtn.addEventListener('click',
+			this.handleLoadMoreClick.bind(this));
 
-        this._view.refs.modalPage.addEventListener('click',
-            this.handleModalControls.bind(this));
+		this._view.refs.list.addEventListener('click',
+			this.handleOpenModal.bind(this));
 
-        this._view.refs.modalPage.addEventListener('click',
-            this.handleFavoritesModalControls.bind(this));
+		this._view.refs.modalPage.addEventListener('click',
+			this.handleModalControls.bind(this));
 
-        this._view.refs.showFavorite.addEventListener('click',
-            this.handleShowFavorite.bind(this));
+		this._view.refs.modalPage.addEventListener('click',
+			this.handleFavoritesModalControls.bind(this));
 
-        this.init();
+		this._view.refs.showFavorite.addEventListener('click',
+			this.handleShowFavorite.bind(this));
 
-    }
+		this.init();
 
-    init() {
-        this._model.addToLocalStorage(this.images)
-    }
+	}
 
-    handleLogoClick() {
-            window.location.reload();
-        }
-        // SUBMIT
+	init() {
+		this._model.addToLocalStorage(this.images)
+	}
 
-    handleFormSumit(e) {
-        e.preventDefault();
-        this.imagesOnPage = []
+	handleLogoClick() {
+		window.location.reload();
+	}
+	// SUBMIT
 
-        this._model.resetCurrentPage();
-        this._view.resetPhotosList();
-        this._model.currentQuery = this._view.refs.input.value;
-        this.handleFetch({
-            query: this._model.currentQuery,
-            page: this._model.currentPage,
-        });
+	handleFormSumit(e) {
+		e.preventDefault();
+		this.imagesOnPage = []
 
-        // this._view.refs.form.reset();
-        this._view.showLoadMoreBtn();
-        this._view.refs.list.classList.remove('js-favorites');
-    }
+		this._model.resetCurrentPage();
+		this._view.resetPhotosList();
+		this._model.currentQuery = this._view.refs.input.value;
+		this.handleFetch({
+			query: this._model.currentQuery,
+			page: this._model.currentPage,
+		});
 
-    handleFetch(params) {
-        this._view.toggleLoader();
+		this._view.showLoadMoreBtn();
+		this._view.refs.list.classList.remove('js-favorites');
+	}
 
-        getImages(params).then(photos => {
-            const markup = this._view.createListItems(photos);
-            this._view.updatePhotosList(markup);
-            this._view.toggleLoader();
-        });
-    }
+	handleFetch(params) {
+		this._view.toggleLoader();
 
-    //LOAD MORE
+		getImages(params).then(photos => {
+			const markup = this._view.createListItems(photos);
+			this._view.updatePhotosList(markup);
+			this._view.toggleLoader();
+		});
+	}
 
-    handleLoadMoreClick() {
-        this._model.incrementCurrentPage();
-        this.handleFetch({
-            query: this._model.currentQuery,
-            page: this._model.currentPage,
-        });
-    }
+	//LOAD MORE
 
-    //MODAL
+	handleLoadMoreClick() {
+		this._model.incrementCurrentPage();
+		this.handleFetch({
+			query: this._model.currentQuery,
+			page: this._model.currentPage,
+		});
+	}
 
+	//MODAL
 
+	handleOpenModal(evt) {
+		if (!evt.target.src) return;
 
-    handleOpenModal(evt) {
-        if (!evt.target.src) return;
+		this._model.disableScroll();
+		this._model.backdropImageInit(evt.target)
+		this.changeColorFavorite(evt.target)
+		this._view.refs.backdrop.classList.add("show-modal")
 
-        this._model.disableScroll();
-        this._model.backdropImageInit(evt.target)
-        this.changeColorFavorite(evt.target)
-        this._view.refs.backdrop.classList.add("show-modal")
+		window.addEventListener('keydown', this.handleModalKeyPress.bind(this));
+	}
 
-        window.addEventListener('keydown', this.handleModalKeyPress.bind(this));
-    }
+	handleModalFalseClick(evt) {
 
-    handleModalFalseClick(evt) {
-            if (!this._view.refs.backdrop.classList.contains('show-modal')) return;
+		if (evt.target.nodeName !== 'IMG' &&
+			evt.target.nodeName !== 'BUTTON' &&
+			!evt.target.classList.contains('page-modal__icons')) {
 
-            const target = evt.target;
-            if (target.parentNode.classList.contains("main")) {
-                this.handleCloseModal();
-            }
+			this.handleCloseModal();
+		}
 
-        }
+	}
 
-				handleModalKeyPress(evt) {
-        const key = evt.code;
-        switch (key) {
-            case 'Escape':
-                this.handleCloseModal();
-                break;
+	handleModalKeyPress(evt) {
+		const key = evt.code;
+		switch (key) {
+			case 'Escape':
+				this.handleCloseModal();
+				break;
 
-            case 'ArrowLeft':
-                this.keyPrev()
-                break;
+			case 'ArrowLeft':
+				this.keyPrev()
+				break;
 
-            case 'ArrowRight':
-                this.keyNext()
-                break;
-        }
-    }
+			case 'ArrowRight':
+				this.keyNext()
+				break;
+		}
+	}
 
-    handleCloseModal() {
-        this._model.backdropCloseModal();
-        this._model.enableScroll();
-        if (!this._view.refs.backdrop.classList.contains('show-modal')) return;
+	handleCloseModal() {
+		this._model.backdropCloseModal();
+		this._model.enableScroll();
+		if (!this._view.refs.backdrop.classList.contains('show-modal')) return;
 
-        this._view.refs.backdrop.classList.remove('show-modal');
-        window.removeEventListener('keydown', this.handleModalKeyPress.bind(this));
-    }
+		this._view.refs.backdrop.classList.remove('show-modal');
+		window.removeEventListener('keydown', this.handleModalKeyPress.bind(this));
+	}
 
-    //FAVORITE
+	//FAVORITE
 
-    handleShowFavorite() {
-        if (!this._view.refs.backdrop.classList.contains('show-modal')) {
-            if (this.images.length === 0) {
-                alert('Вы ничего не добавили в избранное')
-            }
-        }
-        this._view.refs.list.classList.add('js-favorites');
-        this._view.refs.favoriteModalBtn.classList.add('js-favorites__icon');
-        this._view.refs.loadMoreBtn.classList.remove('visible');
-        this._view.refs.list.textContent = '';
-        const markup = this._view.createListItems(this.images);
-        this._view.updatePhotosList(markup);
-    }
-    handleFavoritesModalControls(event) {
-        if (!this._view.refs.list.classList.contains('js-favorites')) return;
+	handleShowFavorite() {
+		if (!this._view.refs.backdrop.classList.contains('show-modal')) {
+			if (this.images.length === 0) {
+				alert('Вы ничего не добавили в избранное')
+			}
+		}
+		this._view.refs.list.classList.add('js-favorites');
+		this._view.refs.favoriteModalBtn.classList.add('js-favorites__icon');
+		this._view.refs.loadMoreBtn.classList.remove('visible');
+		this._view.refs.list.textContent = '';
+		const markup = this._view.createListItems(this.images);
+		this._view.updatePhotosList(markup);
+	}
 
-        const target = event.target;
+	handleFavoritesModalControls(event) {
+		if (!this._view.refs.list.classList.contains('js-favorites')) return;
 
-        if (target.nodeName !== "BUTTON") return;
-        const action = target.dataset.action;
+		const target = event.target;
 
-        switch (action) {
+		if (target.nodeName !== "BUTTON") return;
+		const action = target.dataset.action;
 
-            case 'favorite':
-                this.handleShowFavorite();
-                this.handleCloseModal();
+		switch (action) {
 
-        }
+			case 'favorite':
+				this.handleShowFavorite();
+				this.handleCloseModal();
 
-    }
+		}
 
-    //CONTROLL
+	}
 
-    handleModalControls(event) {
-        event.preventDefault();
-        const target = event.target;
+	//CONTROLL
 
-        if (target.nodeName !== "BUTTON") return;
+	handleModalControls(event) {
+		event.preventDefault();
+		const target = event.target;
 
-        const action = target.dataset.action;
+		if (target.nodeName !== "BUTTON") return;
 
-        switch (action) {
-            case 'next':
-                this.keyNext()
-                break;
+		const action = target.dataset.action;
 
-            case 'prev':
-                this.keyPrev()
-                break;
+		switch (action) {
+			case 'next':
+				this.keyNext()
+				break;
 
-            case 'favorite':
-                const imgUrl = this._view.refs.modalImg.getAttribute("src")
-                const imgId = this._view.refs.modalImg.getAttribute("id")
+			case 'prev':
+				this.keyPrev()
+				break;
 
-                if (this._model.isHasId(imgId, this.images)) {
-                    this.images = this.images.filter(obj => obj.id !== imgId)
-                    this._model.addToLocalStorage(this.images)
+			case 'favorite':
+				const imgUrl = this._view.refs.modalImg.getAttribute("src")
+				const imgId = this._view.refs.modalImg.getAttribute("id")
 
-                    this._view.changeColorFavoriteBtn("#ffffff")
-                    return
-                }
+				if (this._model.isHasId(imgId, this.images)) {
+					this.images = this.images.filter(obj => obj.id !== imgId)
+					this._model.addToLocalStorage(this.images)
 
-                this._view.changeColorFavoriteBtn("#eeed11")
+					this._view.changeColorFavoriteBtn("#ffffff")
+					return
+				}
 
-                const obj = {
-                    id: imgId,
-                    webformatURL: imgUrl,
-                }
+				this._view.changeColorFavoriteBtn("#eeed11")
 
-                this.images.push(obj)
-                this._model.addToLocalStorage(this.images)
-                break;
+				const obj = {
+					id: imgId,
+					webformatURL: imgUrl,
+				}
 
-            case 'close-modal':
-                this.handleCloseModal()
-                break;
-        }
-    }
+				this.images.push(obj)
+				this._model.addToLocalStorage(this.images)
+				break;
 
-    changeColorFavorite(elem) {
-        const imgUrl = elem.getAttribute("src")
-        this._view.refs.modalImg.setAttribute("src", imgUrl)
+			case 'close-modal':
+				this.handleCloseModal()
+				break;
+		}
+	}
 
-        const imgId = elem.getAttribute("id")
-        this._view.refs.modalImg.setAttribute("id", imgId)
+	changeColorFavorite(elem) {
+		const imgUrl = elem.getAttribute("src")
+		this._view.refs.modalImg.setAttribute("src", imgUrl)
 
-        this._model.isHasId(imgId, this.images) ?
-            this._view.changeColorFavoriteBtn("#eeed11") :
-            this._view.changeColorFavoriteBtn("#ffffff")
-    }
+		const imgId = elem.getAttribute("id")
+		this._view.refs.modalImg.setAttribute("id", imgId)
 
-    keyPrev() {
-				const prevImg = this._model.backdropShowPrevImage();
-				if(prevImg === '') return
-        this._view.refs.modalImg.src = prevImg.src
-        this._view.refs.modalImg.id = prevImg.id
-        this.changeColorFavorite(prevImg)
-    }
+		this._model.isHasId(imgId, this.images) ?
+			this._view.changeColorFavoriteBtn("#eeed11") :
+			this._view.changeColorFavoriteBtn("#ffffff")
+	}
 
-    keyNext() {
-        const nextImg = this._model.backdropShowNextImage()
-				if(nextImg === '') return
-				this._view.refs.modalImg.src = nextImg.src
-        this._view.refs.modalImg.id = nextImg.id
-        this.changeColorFavorite(nextImg)
-    }
+	keyPrev() {
+		const prevImg = this._model.backdropShowPrevImage();
+		if (prevImg === '') return
+		this._view.refs.modalImg.src = prevImg.src
+		this._view.refs.modalImg.id = prevImg.id
+		this.changeColorFavorite(prevImg)
+	}
+
+	keyNext() {
+		const nextImg = this._model.backdropShowNextImage()
+		if (nextImg === '') return
+		this._view.refs.modalImg.src = nextImg.src
+		this._view.refs.modalImg.id = nextImg.id
+		this.changeColorFavorite(nextImg)
+	}
 }
